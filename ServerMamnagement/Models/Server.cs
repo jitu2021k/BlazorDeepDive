2c67faa7f9682039fdf29d58ef3617c8f6534ff1
jitendra kumar <jitendra238@gmail.com>
2026-01-12 19:42:54 +0530

    Template Component

--- ServerMamnagement/Models/Server.cs
+++ ServerMamnagement/Models/Server.cs
@@ -1,9 +1,13 @@
-﻿namespace ServerMamnagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerMamnagement.Models
 {
     public class Server
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string City { get; set; } = string.Empty;
         public bool IsOnline { get; set; } = false;
     }
